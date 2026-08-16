58e474f7c6a3604be9d6678488cfd34be8f94e45
0047946-ops <0047946@gmail.com>
2026-08-16 14:35:40 +0800

    Add StallState enum for stall states

---
+++ app/src/main/java/com/currentguardian/model/StallState.kt
@@ -0,0 +1,16 @@
+package com.currentguardian.model
+
+enum class StallState {
+
+    NORMAL,
+
+    POSSIBLE_FRAME_DROP,
+
+    RESPONSIVENESS_DEGRADED,
+
+    STALL_SUSPECTED,
+
+    STALL_CRITICAL,
+
+    UNKNOWN
+}
