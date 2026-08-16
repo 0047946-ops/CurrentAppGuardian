f6d2852b8776161b5193e7d5c9bf4c0207ab21c3
0047946-ops <0047946@gmail.com>
2026-08-16 22:51:33 +0800

    Delete app directory

--- app/src/main/java/com/currentguardian/report/ReportSharePolicy.kt
+++
@@ -1,9 +0,0 @@
-package com.currentguardian.report
-
-data class ReportSharePolicy(
-    val includeDeviceInfo: Boolean = true,
-    val includeNetworkInfo: Boolean = true,
-    val includeTimeline: Boolean = true,
-    val includeBlackBox: Boolean = true,
-    val includePersonalNotes: Boolean = false
-)
--- app/src/main/java/com/currentguardian/testing/TestEventTemplate.kt
+++
@@ -1,8 +0,0 @@
-package com.currentguardian.testing
-
-data class TestEventTemplate(
-    val id: String,
-    val displayName: String,
-    val description: String,
-    val enabled: Boolean = true
-)
